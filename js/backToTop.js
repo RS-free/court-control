ab86de2ef0c31eb7be4930899e6b0dc368c88838
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-02-21 23:57:12 +0200

    add com

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,18 +1,19 @@
+// скіпт для кнопки backToTop
 function backToTop() {
-  let button = $('.back-to-top');
+    let button = $('.back-to-top');
 
-  $(window).on('scroll', () => {
-    if ($(this).scrollTop() >= 400) {
-      button.fadeIn();
-    } else {
-      button.fadeOut();
-    }
-  });
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 400) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
 
-  button.on('click', e => {
-    e.preventDefault();
-    $('html').animate({ scrollTop: 0 }, 1000);
-  });
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({ scrollTop: 0 }, 1000);
+    });
 }
 
 backToTop();
