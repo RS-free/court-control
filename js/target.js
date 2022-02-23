05d12b19ac91570ea6dbbb92f21b9e760027205a
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-02-23 23:52:20 +0200

    add com

--- js/target.js
+++ js/target.js
@@ -1,9 +1,10 @@
+// target
 function externalLinks() {
-            links = document.getElementsByTagName("a");
-                for (i=0; i<links.length; i++) {
-                link = links[i];
-                    if (link.getAttribute("href") && link.getAttribute("rel") == "external")
-                    link.target = "_blank";
-            }
-        }
-        window.onload = externalLinks;
+    links = document.getElementsByTagName('a');
+    for (i = 0; i < links.length; i++) {
+        link = links[i];
+        if (link.getAttribute('href') && link.getAttribute('rel') == 'external')
+            link.target = '_blank';
+    }
+}
+window.onload = externalLinks;
